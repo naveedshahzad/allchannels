94a4b96774eb2177f3be3397b6f863f51e8da4e8
Muhammad Naveed Shahzad <naveedkpr+1@gmail.com>
2025-01-08 01:28:04 +0500

    Add PUBLISH_AT variable

--- upload_video.py
+++ upload_video.py
@@ -87,11 +87,12 @@ def read_github_variable(variable_name):
     response = requests.get(api_url, headers=headers)
 
     if response.status_code >= 200 and response.status_code <= 300:
-        print(f"Variable '{variable_name}' read successfully as '{response.text}' or {response.text}.")
+        print(f"Variable '{variable_name}' read successfully, value = {response.json().get('value')}.")
+        return response.json().get("value")
     else:
-        print(f"error Variable '{variable_name}' read successfully as '{response.text}' or {response.text}.")
         print(f"Failed to update variable. Status code: {response.status_code}")
         print(f"Response: {response.text}")
+        return "error"
 
 def update_publish_at(prevouse_publish_at):
     # Parse the publish_at timestamp
