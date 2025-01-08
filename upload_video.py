82171075cde8a681859e47d533cda6bd29fa6535
Muhammad Naveed Shahzad <naveedkpr+1@gmail.com>
2025-01-08 08:46:13 +0500

    Remove un necessary code

--- upload_video.py
+++ upload_video.py
@@ -34,7 +34,6 @@ def download_file(url, local_path):
 import requests
 
 # Replace with your GitHub details
-#GITHUB_TOKEN = "your_personal_access_token"
 REPO_OWNER = "naveedshahzad"
 REPO_NAME = "lifefule"
 GITHUB_TOKEN = read_file(GITHUB_TOKEN_FILE)  # Your GitHub personal access token
@@ -76,13 +75,6 @@ def read_github_variable(variable_name):
         "Accept": "application/vnd.github+json"
     }
 
-    # Payload with the new value for the variable
-    #payload = {
-    #    "name": variable_name,
-    #    "value": new_value
-    #}
-
-    # Make a PATCH request to update the variable
     response = requests.get(api_url, headers=headers)
 
     if response.status_code >= 200 and response.status_code <= 300:
@@ -108,72 +100,6 @@ def update_publish_at(prevouse_publish_at):
     print(f"{time.isoformat()} hour {time.hour}")
 
     update_github_variable("PUBLISH_AT", time.isoformat())
-    ## Write the updated time to a file
-    #with open("publish_time.txt", "w") as file:
-    #    file.write(time.isoformat())
-
-    ## Print the updated time
-    #print(f"Updated time for next video scheduling {time.isoformat()}")
-
-    ## GitHub Repository Details
-    #REPO_PATH = "."  # Path to your local GitHub repository
-    #GITHUB_TOKEN = read_file(GITHUB_TOKEN_FILE)  # Your GitHub personal access token
-
-    ## Set Git configuration if not already set
-    #os.chdir(REPO_PATH)
-    #subprocess.run(["git", "config", "--global", "user.email", "naveedkpr+1@gmail.com"])
-    #subprocess.run(["git", "config", "--global", "user.name", "Muhammad Naveed Shahzad"])
-
-    ## Add changes to Git
-    #subprocess.run(["git", "add", "publish_time.txt"])
-
-    ## Commit the changes
-    #commit_message = f"Update publish time: {time.isoformat()}"
-    #subprocess.run(["git", "commit", "-m", commit_message])
-
-    ## Push the changes to GitHub
-    #print(f"https://{GITHUB_TOKEN}@github.com/naveedshahzad/lifefule.git")
-def update_publish_at(prevouse_publish_at):
-    # Parse the publish_at timestamp
-    #publish_at = "2025-01-07T22:00:00Z"  # Example input
-    publish_at = prevouse_publish_at  # Example input
-    time = datetime.fromisoformat(publish_at.replace("Z", "+00:00"))
-
-    print(f"{time.isoformat()} hour {time.hour}")
-    # Adjust time based on the hour
-    if time.hour == 22:
-        time += timedelta(hours=15)
-    else:
-        time += timedelta(hours=9)
-    print(f"{time.isoformat()} hour {time.hour}")
-
-    update_github_variable("PUBLISH_AT", time.isoformat())
-    ## Write the updated time to a file
-    #with open("publish_time.txt", "w") as file:
-    #    file.write(time.isoformat())
-
-    ## Print the updated time
-    #print(f"Updated time for next video scheduling {time.isoformat()}")
-
-    ## GitHub Repository Details
-    #REPO_PATH = "."  # Path to your local GitHub repository
-    #GITHUB_TOKEN = read_file(GITHUB_TOKEN_FILE)  # Your GitHub personal access token
-
-    ## Set Git configuration if not already set
-    #os.chdir(REPO_PATH)
-    #subprocess.run(["git", "config", "--global", "user.email", "naveedkpr+1@gmail.com"])
-    #subprocess.run(["git", "config", "--global", "user.name", "Muhammad Naveed Shahzad"])
-
-    ## Add changes to Git
-    #subprocess.run(["git", "add", "publish_time.txt"])
-
-    ## Commit the changes
-    #commit_message = f"Update publish time: {time.isoformat()}"
-    #subprocess.run(["git", "commit", "-m", commit_message])
-
-    ## Push the changes to GitHub
-    #print(f"https://{GITHUB_TOKEN}@github.com/naveedshahzad/lifefule.git")
-    #subprocess.run(["git", "push", "https://{GITHUB_TOKEN}@github.com/naveedshahzad/lifefule.git", "main"])
 
     print("Publish time successfully updated on GitHub!")
 
@@ -187,47 +113,46 @@ def upload_video_to_youtube(title, description, video_path, thumbnail_path, publ
     youtube = build('youtube', 'v3', credentials=credentials)
 
     # Video metadata
-    #request_body = {
-    #    'snippet': {
-    #        'title': title,
-    #        'description': description,
-    #        #'tags': [title.split(" ").first, title.split(" ").last, description.split(" ").last],
-    #        'categoryId': 22,
-    #        #channel_id: "UCPS1Y5fHLenoiRUE3JOhV8w",
-    #    },
-    #    'status': {
-    #        'privacyStatus': 'private',
-    #        'publishAt': publish_at,
-    #        'madeForKids': False,
-    #        'selfDeclaredMadeForKids': False,
-    #    }
-    #}
+    request_body = {
+        'snippet': {
+            'title': title,
+            'description': description,
+            #'tags': [title.split(" ").first, title.split(" ").last, description.split(" ").last],
+            'categoryId': 22,
+            #channel_id: "UCPS1Y5fHLenoiRUE3JOhV8w",
+        },
+        'status': {
+            'privacyStatus': 'private',
+            'publishAt': publish_at,
+            'madeForKids': False,
+            'selfDeclaredMadeForKids': False,
+        }
+    }
 
-    ## Upload video
-    #print(f"Uploading video: {video_path}")
-    #media = MediaFileUpload(video_path, chunksize=-1, resumable=True)
-    #video_request = youtube.videos().insert(
-    #    part='snippet,status',
-    #    body=request_body,
-    #    media_body=media
-    #)
+    # Upload video
+    print(f"Uploading video: {video_path}")
+    media = MediaFileUpload(video_path, chunksize=-1, resumable=True)
+    video_request = youtube.videos().insert(
+        part='snippet,status',
+        body=request_body,
+        media_body=media
+    )
 
-    #response = video_request.execute()
-    #print(f"Video uploaded: {response['id']}")
-    update_publish_at(publish_at)
-    #if 'id' in response:
-    #    print(f"Video uploaded: {response['id']}")
+    response = video_request.execute()
+    print(f"Video uploaded: {response['id']}")
+    if 'id' in response:
+        print(f"Video uploaded: {response['id']}")
 
-    #    # Upload thumbnail
-    #    print(f"Uploading thumbnail: {thumbnail_path}")
-    #    youtube.thumbnails().set(
-    #        videoId=response['id'],
-    #        media_body=MediaFileUpload(thumbnail_path)
-    #    ).execute()
-    #    print(f"Thumbnail uploaded.")
-    #    update_publish_at(publish_at)
-    #else:
-    #    print("Video upload failed, no ID in response.")
+        # Upload thumbnail
+        print(f"Uploading thumbnail: {thumbnail_path}")
+        youtube.thumbnails().set(
+            videoId=response['id'],
+            media_body=MediaFileUpload(thumbnail_path)
+        ).execute()
+        print(f"Thumbnail uploaded.")
+        update_publish_at(publish_at)
+    else:
+        print("Video upload failed, no ID in response.")
 
 if __name__ == "__main__":
     # Read metadata from files
